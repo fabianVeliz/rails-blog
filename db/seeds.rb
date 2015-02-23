9617ba16195059645b2734adfa4eb6e270be40d1
fabianVeliz <velizfabianhoracio@gmail.com>
2015-02-23 15:58:56 -0300

    Update seeds file: Add user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
-  puts 'Admin User: '
-  admin = Admin.create(email: 'admin@admin.com', password: 123123123)
-  puts 'Email: ' + admin.email
-  puts 'Pass: 123123123'
+puts '--------------'
+puts '**Admin**'
+puts '--------------'
+admin = Admin.create(email: 'admin@admin.com', password: 123123123)
+puts 'Email: ' + admin.email
+puts 'Pass: 123123123'
+
+puts '--------------'
+puts '**User**'
+puts '--------------'
+user = User.create(email: 'user@user.com', password: 123123123)
+puts 'Email: ' + user.email
+puts 'Pass: 123123123'
